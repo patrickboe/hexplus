99d2997ba3c0d22819980f7073b4255dfd6303d5
patrick <patrickboe@gmail.com>
2010-11-21 16:57:34 -0500

    added deploy, hosting information

---
+++ README.markdown
@@ -0,0 +1,31 @@
+hexpl.us
+Copyright (c) 2010 Patrick Boe  
+Version: 0.1 (11/21/2010)  
+Dual licensed under the MIT and GPL licenses:  
+http://www.opensource.org/licenses/mit-license.php  
+http://www.gnu.org/licenses/gpl.html
+
+##Set Up a Dev Environment
+Clone the repository, then change to the project's src/build directory.
+
+You need Paver and pyYAML. To install what you don't have yet, run:
+
+	pip install -r requirements.txt
+
+You also need to install the [Google App Engine SDK for Python](http://code.google.com/appengine/downloads.html#Google_App_Engine_SDK_for_Python).
+
+Make a copy of `/src/build/my_example.yaml` named `my.yaml`. Customize it for your environment.
+ 
+##Build
+	
+from the project root do:
+	
+	paver build
+	
+##Run Locally
+
+	paver run
+ 
+##Deploy
+	
+	paver deploy
---
+++ pavement.py
@@ -0,0 +1,98 @@
+from paver.easy import *
+import string
+import re
+import sys
+import subprocess
+from datetime import date
+
+deployDir = path("deploy")
+srcDir = path("src")
+buildDir = srcDir / "build"
+sys.path.append(buildDir.abspath())
+from buildconfig import loadConf
+
+options(
+        version=sh("git describe --tags",capture=True).strip(),
+        version_date=date.today(),
+        targetEnv="local"
+        )
+
+def appEngineCommand(pyname, args=[]):
+    def report(status):
+        sys.stderr.writelines(["%s: " % status, " ".join(cmd)])
+    cmd=[options.app_engine_python_executable, 
+        path(options.app_engine_path) / pyname]
+    cmd.extend(args)
+    cmd.append(deployDir.abspath())
+    try:
+        retcode=subprocess.call(cmd)
+        if retcode < 0:
+            report("terminated")
+        else:
+            report("completed")
+    except KeyboardInterrupt:
+        pass
+
+@task 
+def auto(): 
+    "runs on every paver call"
+    loadConf(options)     
+    options.version_name=options.version.replace(".","-") + (options.debug and "-dev" or "")
+
+@task
+def build():
+    "transform source code into a deployable google app"
+    def combine(mediaPath, in_files, out_file, in_type='js'):
+        "combine text files into one"
+        out = open(mediaPath / ('%s.%s' % (out_file,in_type)), 'w')
+        for f in in_files:
+            f=mediaPath / ('%s.%s'%(f,in_type))
+            with open(f) as fh:
+                data = fh.read() + '\n'
+            f.remove()
+            out.write(data)
+            print ' + %s' % f
+        out.close()
+    def configureFiles():
+        """in all code files in the deploy directory, substitute strings of format 
+        $CONF_[a key from pavement options] with the option's value"""
+        def applyConfig(curPath):
+            for f in [a for a in curPath.files() if re.search("\.(yaml|js|html|djml|css|py)$",a)]:
+                with open(f,'r') as fh:
+                    subbed=string.Template(fh.read()).safe_substitute(config)
+                with open(f,'w') as fh:
+                    fh.write(subbed)
+            for d in curPath.dirs():
+                applyConfig(d)
+        config=dict([("CONF_%s" % k,v) for (k,v) in options.iteritems()])
+        applyConfig(deployDir)
+    deployDir.rmtree()
+    mediaDir=deployDir/"media"
+    (srcDir / "content").copytree(deployDir / "content")
+    (srcDir / "media").copytree(mediaDir)
+    (buildDir / "app.yaml").copy(deployDir / "app.yaml")
+    options.update(options.get(options.targetEnv))
+    configureFiles()
+    combineName='app-%s' % options.version_name
+    combine(mediaDir/"js",
+            [
+             'jquery.textchange.min',
+             'jquery.ba-hashchange.min',
+             'hex'
+             ],combineName)
+    
+@task
+@needs('build')
+def run():
+    "start local google app engine server for this app"
+    appEngineCommand("dev_appserver.py",["--port=%d" % options.app_engine_portnum])
+
+@task
+def setlive():
+    options.targetEnv="live"
+
+@task
+@needs('setlive','build')
+def deploy():
+    "put the current application live"
+    appEngineCommand("appcfg.py",["update"])
